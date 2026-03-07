87694e19b2305ccfe38c0f9fddbf2a77d6c6fa3b
saurydeep singh <saurydeepsingh231305@gmail.com>
2026-03-08 00:14:54 +0530

    Add files via upload

---
+++ UserDetailsApplication.java
@@ -0,0 +1,14 @@
+package com.User.details.User.Details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDetailsApplication.class, args);
+//		System.out.print("Hello Saurydeep Singh");
+	}
+
+}
